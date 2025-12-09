0d31e38813904b6fd7b548a26ab4e03143d7dd99
lydiablott <lynn.limbach@aol.de>
2025-12-09 02:45:00 +0100

    Build for GitHub Pages into docs

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-//for domains or aliases
-//dont change anything until designing github page
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/amnh-application-lynnlimbach/',  // GitHub Pages base path
+  build: {
+    outDir: 'docs',                        // build output goes into docs/
+  },
 })
